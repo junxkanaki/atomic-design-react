17c925c83d1619999e4dc99a8e24178ffabce702
Jun Kanaki <93533041+junxkanaki@users.noreply.github.com>
2022-06-29 15:27:52 +0900

    Organismの作成

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
 
 export default function App() {
+  const user = {
+    name: "Juna",
+    image: "https://source.unsplash.com/IuJc2qh2TcA",
+    email: "abcd@eee.com",
+    phone: "090-1234-5678",
+    company: {
+      name: "Juna株式会社"
+    },
+    website: "https://google.com"
+  };
   return (
     <div className="App">
       <PrimaryButton>Test</PrimaryButton>
       <SecondaryButton>Search</SecondaryButton>
       <br />
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
